bf1122c5078a78103dfd60576ae57be60bcc08da
Kevin Webster Revalde <kevinwebsterr2016@gmail.com>
2024-01-03 01:11:07 +0800

    routes

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
+
+export const Navbar = () => {
+  return (
+    <header>
+      <div className="wrapper">
+        <div className="header_con">
+          <div className="main_logo">G-UP</div>
+
+          <nav>
+            <NavLink to="/">Home</NavLink>
+
+            <NavLink to="/login">Login</NavLink>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+  return (
+    <>
+      <h1>Banner</h1>
+      <h1>Check it Now</h1>
+    </>
+  );
+};
